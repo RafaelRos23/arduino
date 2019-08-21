2e307c092a144fec5464b85c02010bf2f20e1f3f
RafaelRos23 <54335548+RafaelRos23@users.noreply.github.com>
2019-08-20 22:18:28 -0300

    adicionei arquivos via upload super maquina

---
+++ super maquina/superMaquina.ino
@@ -0,0 +1,17 @@
+/**
+   SEQUENCIAL DE LEDS (EFEITO SUPER MAQUINA)
+   @AUTOR - RAFAEL OLIVEIRA DE SOUZA
+*/
+
+void setup() {
+
+  pinMode(13, OUTPUT);
+
+}
+
+void loop() {
+
+  digitalWrite (13, HIGH);
+
+  
+}
