79b6e2cb4c66e84c6d3cdbc8e82649c4a1597f67
RafaelRos23 <54335548+RafaelRos23@users.noreply.github.com>
2019-09-03 22:20:08 -0300

    portasLogicas

---
+++ portasLogicasOr/portasLogicasOr.ino
@@ -0,0 +1,18 @@
+/**
+   ESTUDO DAS PORTAS LÓGICAS
+   AUTHOR RAFAEL OLIVEIRA DE SOUZA
+*/
+
+void setup() {
+  pinMode (13, OUTPUT);
+  pinMode (2, INPUT);
+  pinMode(3, INPUT);
+}
+
+void loop() {
+  if (digitalRead(2) == HIGH || digitalRead(3)== HIGH){
+    digitalWrite(13, HIGH);}
+    else {digitalWrite (13, LOW); }
+  
+  
+}
