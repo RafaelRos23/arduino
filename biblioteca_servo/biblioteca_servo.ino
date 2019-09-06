44acaaa2dcfe6579ee89bb44e69207f75480175c
RafaelRos23 <54335548+RafaelRos23@users.noreply.github.com>
2019-09-05 22:18:39 -0300

    bibliotecas_servo_exemplo

---
+++ biblioteca_servo/biblioteca_servo.ino
@@ -0,0 +1,18 @@
+/**
+ * USO DE BIBLIOTECAS (SERVO)  
+ * @author RAFAEL OLIVEIRA DE SOUZA
+ */
+
+ #include <Servo.h>
+ Servo servo1;
+void setup() {
+  servo1.attach(9);
+}
+
+void loop() {
+  servo1.write(180);
+  delay(1000);
+  servo1.write(0);
+  delay(1000);  
+
+}
