844558bcb0a100a1568b99c640e2064a82c6f10c
RafaelRos23 <54335548+RafaelRos23@users.noreply.github.com>
2019-10-08 22:23:43 -0300

    vetor array

---
+++ array/array.ino
@@ -0,0 +1,17 @@
+/**
+ * Array (vetor)
+ * @author Rafael Oliveira de Souza
+ */
+
+void setup() {
+  Serial.begin (9600);
+  String times [4] = {"Corinthians","São Paulo", "Santos", "Palmeiras"};
+  Serial.println(times[2]);
+ 
+
+}
+
+void loop() {
+  
+
+}
